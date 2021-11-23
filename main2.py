9a3229dc8497c821062c28bd0ac99a0baf98934a
fdavin <fransdavin19@gmail.com>
2021-11-23 11:26:20 +0700

    add tester

---
+++ main2.py
@@ -0,0 +1,27 @@
+import codeTokenizer as ctok
+import cyk
+import sys
+
+# Load Chomsky Normal Form
+if len(sys.argv) > 1:
+    modelPath = str(sys.argv[1])
+else:
+	modelPath = 'cnf.txt'
+	
+cyk.LoadCNF(modelPath)
+
+# Tokenize
+tokenizedCode = ctok.tokenizeInput("input.txt")
+
+#print(tokenizedCode)
+
+# CYK
+table = cyk.cyk(tokenizedCode)
+
+#for x in table:
+#    print(x)
+
+if (cyk.checkValidity(table, "S")):
+    print("Verdict accepted! Compile success!")
+else:
+    print("Compile error, wrong syntax!")
