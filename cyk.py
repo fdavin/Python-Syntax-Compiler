60459bed893e52b67afea3fc6c63d89dd46c7bf6
roastland <tfpruslanali@gmail.com>
2021-11-24 15:07:06 +0700

    update comment

--- cyk.py
+++ cyk.py
@@ -3,7 +3,7 @@ import re
 # Regex List
 regexList = [r'[A-z0-9]*', r'[0-9]*', r'[A-Za-z_][A-Za-z_0-9]*']
 
-# Maps regex into valid key
+# Mapping regex menjadi key grammar yang valid
 regexMap = {
     r'[A-z0-9]*' : ["string"],
     r'[0-9]*' : ["number"],
@@ -11,7 +11,7 @@ regexMap = {
 }
 
 grammar = {}
-# Load CNF File
+# Load CNF menjadi dictionary
 def LoadCNF(rulesPath):
     file = open(rulesPath).read()
     rawRules = file.split('\n')
@@ -30,12 +30,12 @@ def LoadCNF(rulesPath):
 def cyk(tokens):
     n = len(tokens)
     cykTable = [[[] for j in range(i)] for i in range(n,0,-1)]
-    # Initialize first row cykTable
+    # Inisialisasi cykTable
     for i in range(n):
-        # If key valid
+        # Jika key grammar valid
         try:
             cykTable[0][i].extend(grammar[tokens[i]])
-        # If key invalid
+        # Jika tidak valis
         except KeyError:
             for pattern in regexList:
                 if(re.match(pattern, tokens[i])):
@@ -44,16 +44,16 @@ def cyk(tokens):
                             cykTable[0][i].extend(grammar[regexType])
                         except KeyError:
                             continue 
-    # Iterate Bottom Up
+    # Iterasi Bottom-Up
     for i in range(1,n):
         for j in range(n-i):
             for k in range(i):
-                # Test for combinations
+                # Tes kombinasi grammar
                 for prod1 in cykTable[i-k-1][j]:
                     for prod2 in cykTable[k][j+i-k]:
                         try:
                             cykTable[i][j]=grammar[prod1+prod2]
                         except KeyError:
                             continue
-    # Check if tokens can be reached from startsymbol
+    # Mengembalikan True jika tokens bisa dibentuk dari startsymbol "S"
     return ("S" in cykTable[-1][-1])
